cc26b7fd340d44256b09feb14a9cc1031ac46059
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-02 17:44:48 +0900

    「クエリパラメータ」の章を終えた

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Union
 from fastapi import FastAPI
 
 class ModelName(str, Enum):
@@ -7,6 +8,10 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"Hello", "World"}
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -22,3 +27,21 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
