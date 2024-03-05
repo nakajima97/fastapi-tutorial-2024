9804900fdbd0802d3d77653ac5b163f931e31244
nakajima97 <kituneudon97@gmail.com>
2024-03-05 09:23:13 +0900

    「リクエストボディ」の章をクリア

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,11 +1,18 @@
 from enum import Enum
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
@@ -45,3 +52,15 @@ async def read_user_item(
     if not short:
         item.update({"description": "This is an amazing item that has a long description"})
     return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
