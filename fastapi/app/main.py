41d05528edb2770fa017604a210c16f34661985a
nakajima97 <kituneudon97@gmail.com>
2024-03-07 09:20:06 +0900

    「ボディ - フィールド」のコードを写経

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,18 +1,16 @@
 from typing import Union
 from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-
 class Item(BaseModel):
     name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
+    description: Union[str, None] = Field(default=None, title="The description of the item", max_length=300)
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: float = None
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item = Body(embed=True)):
+async def update_item(item_id: int, item: Union[Item, None] = Body(default=None, embed=True)):
     results = {"item_id": item_id, "item": item}
     return results
