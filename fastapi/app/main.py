2897aa1d0fb7c8fa996df00bf7dd9f3cac06db2c
nakajima97 <kituneudon97@gmail.com>
2024-03-08 09:10:00 +0900

    「クッキーのパラメータ」を写経

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,29 +1,8 @@
 from typing import Union
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from fastapi import Cookie, FastAPI
 
-app = FastAPI()
+app = FastAPI();
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
-@app.put("/items/{item_id}")
-async def update_item(
-    item_id: int,
-    item: Union[None, Item] = Body(
-        default=None,
-        examples=[
-            {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-            }
-        ],
-    ),
-):
-    results = {"item_id": item_id, "item": item}
-    return results
+@app.get("/items/")
+async def read_items(ads_id: Union[str, None] = Cookie(default=None)):
+    return {"ads_id": ads_id}
