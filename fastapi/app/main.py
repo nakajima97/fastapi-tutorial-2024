d0e508f3a4432323b5bf6019fb00223aa8829359
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-08 21:01:55 +0900

    「レスポンスモデル」のコードを写経

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,33 @@
-from typing import Union, List
-from fastapi import FastAPI, Header
+from typing import List, Union
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/items/")
-async def read_items(x_token: Union[List[str], None] = Header(default=None)):
-    return {"X-Token values": x_token}
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: float = 10.5
+    tags: List[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+    response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+@app.get(
+    "/items/{item_id}/public",
+    response_model=Item,
+    response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
