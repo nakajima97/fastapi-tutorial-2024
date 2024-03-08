ac1d5eb0fc2ba641b77e20d63dc7b8b54186d48b
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-08 21:50:45 +0900

    「フォームデータ」のコードを写経

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str):
-    return {"name": name}
+@app.post("/login/")
+async def login(username: [None, str] = Form(default=None), password: [None, str] = Form(default=None)):
+    return {"username": username, "password": password}
